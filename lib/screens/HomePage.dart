62f48bafda3a952dcd85aaa9a145303838dc327b
Amber8-1 <163436403+Amber8-1@users.noreply.github.com>
2024-05-19 18:06:56 +0530

    Update HomePage.dart

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:test_app/screens/ChatPage.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -42,25 +42,11 @@ class _HomePageState extends State<HomePage> {
       "sent": false
     },
     {
-      "name": "Shatrughna",
-      "Message": "ALl the best bhrata shree!",
-      "time": "11:12",
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
       "readStatus": true,
-      "sent": false
-    },
-    {
-      "name": "Hanuman",
-      "Message": "Jai Shree Ram. On your command, my lord!",
-      "time": "11:11",
-      "readStatus": true,
-      "sent": false
-    },
-    {
-      "name": "Sita",
-      "Message": "I am waiting, Come soon!",
-      "time": "11:11",
-      "readStatus": true,
-      "sent": false
+      "sent": true
     },
     {
       "name": "Dashrath",
@@ -69,90 +55,194 @@ class _HomePageState extends State<HomePage> {
       "readStatus": true,
       "sent": true
     },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
+    {
+      "name": "Dashrath",
+      "Message": "All the best!",
+      "time": "11:20",
+      "readStatus": false,
+      "sent": false
+    },
   ];
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("What's UP"),
+              Row(
+                children: [
+                  Icon(Icons.search),
+                  PopupMenuButton<String>(
+                      onSelected: (value) => {print(value)},
+                      itemBuilder: (BuildContext context) =>
+                          <PopupMenuEntry<String>>[
+                            PopupMenuItem(
+                              value: "New Group",
+                              child: Text("New Group"),
+                            ),
+                            PopupMenuItem(
+                              value: "New Broadcast",
+                              child: Text("New Broadcast"),
+                            ),
+                            PopupMenuItem(
+                              value: "Linked devices",
+                              child: Text("Linked devices"),
+                            ),
+                            PopupMenuItem(
+                              value: "Starred message",
+                              child: Text("Starred message"),
+                            ),
+                            PopupMenuItem(
+                              value: "Payment",
+                              child: Text("Payment"),
+                            ),
+                            PopupMenuItem(
+                              value: "Settings",
+                              child: Text("Settings"),
+                            ),
+                          ])
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: Column(
           children: [
-            Text("What's UP"),
+            Expanded(
+              child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: chats.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(chats[index]["name"]),
+                      subtitle: chats[index]["sent"]
+                          ? Row(
+                              children: [
+                                const Icon(Icons.done_all),
+                                Text(
+                                  chats[index]["Message"],
+                                  style: const TextStyle(
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                              ],
+                            )
+                          : Text(
+                              chats[index]["Message"],
+                              style: const TextStyle(
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: index % 2 == 0
+                            ? Image.asset("assets/srk.png")
+                            : Image.asset("assets/srk1.png"),
+                      ),
+                      trailing: Text(chats[index]["time"]),
+                      onTap: () => Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return ChatPage(
+                          name: chats[index]["name"],
+                          image: index % 2 == 0
+                              ? "assets/srk.png"
+                              : "assets/srk1.png",
+                        );
+                      })),
+                    );
+                  }),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Icon(Icons.search),
-                PopupMenuButton<String>(
-                    onSelected: (value) => print(value),
-                    itemBuilder: (BuildContext context) =>
-                        <PopupMenuEntry<String>>[
-                          PopupMenuItem(
-                            value: "New Group",
-                            child: Text("New Group"),
-                          ),
-                          PopupMenuItem(
-                            value: "New Broadcast",
-                            child: Text("New Broadcast"),
-                          ),
-                          PopupMenuItem(
-                            value: "Linked devices",
-                            child: Text("Linked devices"),
-                          ),
-                          PopupMenuItem(
-                            value: "Starred message",
-                            child: Text("Starred message"),
-                          ),
-                          PopupMenuItem(
-                            value: "Payment",
-                            child: const Text("Payment"),
-                          ),
-                          PopupMenuItem(
-                            value: "Settings",
-                            child: Text("Settings"),
-                          ),
-                        ])
+                ElevatedButton(
+                  onPressed: () {
+                    _scrollController.animateTo(
+                      0,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text("Go to Top"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _scrollController.animateTo(
+                      _scrollController.position.maxScrollExtent,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text("Go to Bottom"),
+                ),
               ],
             ),
           ],
-        ),
-      ),
-    );
-    body:
-    ListView.builder(
-        itemCount: chats.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(chats[index]["name"]),
-            subtitle: chats[index]["sent"]
-                ? Row(
-                    children: [
-                      const Icon(Icons.done_all),
-                      Text(
-                        chats[index]["Message"],
-                        style: const TextStyle(overflow: TextOverflow.ellipsis),
-                      ),
-                    ],
-                  )
-                : Text(
-                    chats[index]["Message"],
-                    style: const TextStyle(overflow: TextOverflow.ellipsis),
-                  ),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(40),
-              child: index % 2 == 0
-                  ? Image.asset("assets/srk.png")
-                  : Image.asset("assets/srk1.png"),
-            ),
-            trailing: Text(chats[index]["time"]),
-            onTap: () =>
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return ChatPage(
-                name: chats[index]["name"],
-                image: index % 2 == 0 ? "assets/srk.png" : "assets/srk1.png",
-              );
-            })),
-          );
-        });
+        ));
   }
 }
