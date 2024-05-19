d3aca1ade307488ec62edb3cbf0681447a876858
Amber8-1 <163436403+Amber8-1@users.noreply.github.com>
2024-05-19 17:43:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/screens/HomePage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,4 +15,53 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
+}
+
+class WhatsAppHomePage extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Container(
+        padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                _scrollController.animateTo(
+                    _scrollController.position.maxScrollExtent,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.fastOutSlowIn);
+              },
+              child: Icon(Icons.arrow_downward_sharp),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                _scrollController.animateTo(
+                    _scrollController.position.minScrollExtent,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.fastOutSlowIn);
+              },
+              child: Icon(Icons.arrow_upward_sharp),
+            )
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: 7, // Assuming there are 50 chat items
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Chat $index'),
+          );
+        },
+      ),
+    );
+  } // Add the missing closing curly brace here
 }
